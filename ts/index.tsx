b719a9a577f7daef57dcad9435a39a78d02cc0e5
Kishore <kishore.iiitn@gmail.com>
2017-06-13 22:59:19 +0530

    Webpack workflow.

---
+++ ts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import kishore from './kishore';
+
+let App = ()=>{
+	return <h1>Hello Jagadeesh</h1>;
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById("app")
+);
