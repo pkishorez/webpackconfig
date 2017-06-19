14080f2e9ab7a96610a88bb717b16bfb83dc0cd5
Kishore <kishore.iiitn@gmail.com>
2017-06-19 23:43:45 +0530

    Todo's task

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import kishore from './kishore';
 
-let App = ()=>{
-	return <h1>Hello Jagadeesh</h1>;
+interface IProps {};
+interface IState {
+	todos: any[],
+	filter: "ALL" | "COMPLETE" | "ACTIVE"
+};
+
+class Todos extends React.Component<IProps, IState> {
+
+	private input: any;
+
+	constructor() {
+		super();
+		this.state = {
+			todos: [],
+			filter: "ALL"
+		};
+		this.addTodo = this.addTodo.bind(this);
+	}
+
+	addTodo(e:any) {
+		e.preventDefault();
+
+		this.setState({
+			todos: [
+				...this.state.todos,
+				this.input.value
+			]
+		});
+		this.input.value = "";
+	}
+
+	render() {
+		let todos = this.state.todos;
+		let todosDisplay = todos.map((todo, i)=>{
+			return <li key={i}>{todo}</li>;
+		});
+		return <div>
+			<h1>Todos</h1>
+			<form onSubmit={this.addTodo}>
+				<input type="text" ref={(ref)=>{this.input = ref;}}/>
+			</form>
+			<ul>
+			{todosDisplay}
+			</ul>
+		</div>;
+	}
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById("app")
-);
+	<Todos />, document.getElementById("app")
+)
